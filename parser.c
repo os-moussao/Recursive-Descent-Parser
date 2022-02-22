55ea81ca69e40b00181a3e1e6ce0f55ef909388a
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-22 23:29:16 +0100

    update parser
    - empthy code for now

--- parser.c
+++ parser.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-
-#define MAXSIZE 1024
-
-
-
-int main(void)
-{
-	char	expr[MAXSIZE];
-
-	printf("Enter expression: ");
-	scanf("%[^\n]", expr);
-	printf("%s\n", expr);
-}
+#include "rdp_calc.h"
