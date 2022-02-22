67f31ae154724ea15558bb0925984149f53c1026
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-23 00:00:20 +0100

    Add lexer header file
    - Added Node, lexer_t structs
    - Added lexer prototype
    - Added list utils prototypes

---
+++ lexer.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "rdp_calc.h"
+
+typedef struct Node {
+	token_t tok;
+	int     val; /* if tok == number */
+	struct Node *next;
+	struct Node *prev;
+} Node;
+
+typedef struct lexer_s {
+	Node *top;
+	Node *bottom;
+	int len;
+} lexer_t;
+
+/**
+ * Lexer function
+ */
+lexer_t	*lexer(char *expr);
+
+/**
+ * list utils
+ */
+lexer_t	*new_list(void);
+Node *new_node(token_t tok, int val);
+Node *push_front(lexer_t *tokens, Node *new);
+Node *push_back(lexer_t *tokens, Node *new);
+Node *del_front(lexer_t *tokens);
+Node *del_back(lexer_t *list);
+void list_clear(lexer_t *list);
