65a77f9461c761759f6f536458a606cac7316afc
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-23 20:23:26 +0100

    Update parser
    - Added Grammar
    - Added parser function

--- parser.c
+++ parser.c
@@ -1,1 +1,24 @@
 #include "rdp_calc.h"
+
+// Grammar:
+// <expression>	::= <term> {("+" | "-") <term>}
+// <term>		::= <factor> {("*" | "/") <factor>}
+// <factor>		::= <number> | "(" <expression> ")"
+// <number>		::= positive integer
+
+
+// global variable holding the current Symbol
+Node *Symp;
+
+// Parser function
+ASTreeNode_t *parser(lexer_t *tokens)
+{
+	initSymp;
+
+	ASTreeNode_t *ASTree = expression();
+	if (ASTree == NULL) {
+		error("parser: syntax error\n");
+	}
+
+	return ASTree;
+}
