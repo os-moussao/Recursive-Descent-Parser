f76c09e8ad80e9974999f8ff6c3a46de60129940
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-23 20:36:34 +0100

    Update rec-desc-parser calculator header file
    - Including lexer, parser, display headers
    - Adding core function declaration

--- include/rdp_calc.h
+++ include/rdp_calc.h
@@ -6,8 +6,15 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <stdarg.h>
+#include <limits.h>
+
 #include "lexer.h"
+#include "parser.h"
+#include "display.h"
 
 #define MAXSIZE 1024
 
-void dispTokens(Node *tokens);
+lexer_t       *lexer(char *expr);
+ASTreeNode_t  *parser(lexer_t *tokens);
+int           eval(ASTreeNode_t *ASTree);
