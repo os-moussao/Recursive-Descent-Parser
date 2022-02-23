d9c60b1853a1da070410d3a25ae33bd4767e2bc5
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-23 20:31:23 +0100

    Update display.c
    - Updated displayTokens function

--- display.c
+++ display.c
@@ -1,13 +1,21 @@
 #include "rdp_calc.h"
 
-const char *names[] = {
+const char *name[] = {
 	"number", "opar", "cpar", "plus", "minus", "mult", "div_", "unknown"
 };
 
-void dispTokens(Node *tokens)
+const char *lexeme[] = {
+	"", "(", ")", "+", "-", "*", "/", "unknown"
+};
+
+void displayTokens(Node *tokens)
 {
+	printf("Tokens Begin:\n");
 	while (tokens) {
-		printf("%s\n", names[tokens->tok]);
+		printf("%s\n", name[tokens->tok]);
 		tokens = tokens->next;
 	}
+	printf("Tokens End:\n");
+	fflush(stdout);
+}
 }
