8a85d079c193190284f242e36c503d6ce778fdd1
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-23 20:32:18 +0100

    Added display header file
    - Added colors
    - Added funtion declarations

---
+++ include/display.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "rdp_calc.h"
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+void displayTokens(Node *tokens);
+void displayExpression(ASTreeNode_t *tree);
+void displayASTree(ASTreeNode_t *tree);
+void error(char *format, ...);
