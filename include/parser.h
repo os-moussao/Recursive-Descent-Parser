ad36db5f677065bb5e6190b881ed4f88815f9bfe
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-23 20:17:42 +0100

    Add parser header file
    - Added ASTreeNode_t struct
    - Added prototypes
    - Added helpful defines

---
+++ include/parser.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "rdp_calc.h"
+
+#define initSymp Symp = tokens->top
+#define nextSymp Symp = Symp->next
+#define Sym Symp->tok
+#define Val Symp->val
+#define none 0
+
+typedef struct ASTreeNode {
+	token_t type;
+	int     value;
+	struct ASTreeNode *left;
+	struct ASTreeNode *right;
+}				ASTreeNode_t;
+
+ASTreeNode_t *parser(lexer_t *tokens);
+
+ASTreeNode_t *expression();
+ASTreeNode_t *term();
+ASTreeNode_t *factor();
+
+// constructor
+ASTreeNode_t *ast_new(token_t type, int value, ASTreeNode_t *left, ASTreeNode_t *right);
+
+// destructor
+void	ast_clear(ASTreeNode_t *root);
