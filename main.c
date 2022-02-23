368c0b76e8cbd9631594f701fa5e27dfc12bb4b7
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-23 20:38:44 +0100

    Update main program
    - Grouping the logic: prompt, lex, parse, eval, repeat

--- main.c
+++ main.c
@@ -12,20 +12,38 @@ int main(void)
 {
 	char	expr[MAXSIZE];
 	lexer_t *tokens;
+	ASTreeNode_t *AbstractSyntaxTree;
 
 	while (true)
 	{
 		prompt(expr);
+
+		if (!*expr)
+			continue ;
 
 		tokens = lexer(expr);
 
 		if (!tokens)
 			continue ;
 
-		// * debug
-		dispTokens(tokens->top);
-		// * debug
+		if (!tokens->len) {
+			list_clear(tokens);
+			continue ;
+		}
 
+		AbstractSyntaxTree = parser(tokens);
+
+		if (!AbstractSyntaxTree)
+			continue ;
+
+		// displays
+		displayASTree(AbstractSyntaxTree);
+		displayExpression(AbstractSyntaxTree);
+		printf("%s\nResult: %d\n", KGRN, eval(AbstractSyntaxTree));
+		printf(KNRM);
+
+		// Clear memory
 		list_clear(tokens);
+		ast_clear(AbstractSyntaxTree);
 	}
 }
