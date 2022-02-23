26acc7f098b3a4f5614bd5c7793f9e279cf556ad
os-moussao <78625018+os-moussao@users.noreply.github.com>
2022-02-23 09:01:14 +0100

    Update main.c
    - Added prompt
    - Using dispTokens

--- main.c
+++ main.c
@@ -1,13 +1,31 @@
 #include "rdp_calc.h"
 
+void prompt(char *expr)
+{
+	printf("Enter expression: ");
+	fflush(stdout);
+	int nbytes = read(0, expr, MAXSIZE);
+	expr[nbytes] = '\0';
+}
+
 int main(void)
 {
 	char	expr[MAXSIZE];
+	lexer_t *tokens;
 
 	while (true)
 	{
-		printf("Enter expression: ");
-		scanf("%[^\n]", expr);
-		printf("%s\n", expr);
+		prompt(expr);
+
+		tokens = lexer(expr);
+
+		if (!tokens)
+			continue ;
+
+		// * debug
+		dispTokens(tokens->top);
+		// * debug
+
+		list_clear(tokens);
 	}
 }
